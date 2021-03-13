9c1acba13771c01fb9b4c7b12b162231c8d38c25
Matt Mannion <mattpmannion@gmail.com>
2021-03-13 13:28:38 -0500

    parcel 1.12.3 locked

--- src/typescript/index.tsx
+++ src/typescript/index.tsx
@@ -1,10 +1,5 @@
-// core-js/stable and reg. runtime
-// are needed for babel
 import 'core-js/stable';
-// only import core-js this once
 import 'regenerator-runtime/runtime';
-// reg. runtime is for poly-filling
-// 'async' calls, import when needed
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
